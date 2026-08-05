b90face7b8277998c28b4aeb7e346d8cb0f38144
abbydoag <abby.donis.a@gmail.com>
2026-08-04 21:11:03 -0600

    Base medlodia

--- GLACIER.py
+++ GLACIER.py
@@ -13,13 +13,21 @@ pitches3 = [CS2,ES2,CS3,GS2,ES2,GS2,CS3,ES3]
 notes = len(pitches3)
 duration3 = [QN]*notes
 
-pitches4 = [FS2,E2,FS4,E4,D4,D4,CS3]
+pitches4 = [FS2,E2,FS3,E3,D3,CS3,B3]
 notes=len(pitches4)
-duration4= [WN*2.2]*notes
+duration4= [WN*2.0]*notes
 
-pitches5 = [GS4,GS4]
+pitches5 = [GS3,D4,GS4]
 notes=len(pitches5)
-duration5= [WN*2.3]*notes
+duration5= [WN*2.1]*notes
+
+pitches6 = [B2,E3,D3,E3,B2,E3,D3,E3,A2,D3,CS3,D3,A2,D3,CS3,D3,G2,D3,B2,D3,G2,D3,B2,D3,G2,D3,B2,D3,G2,D3,B2,D3]
+notes = len(pitches6)
+duration6 = [QN]*notes
+
+pitches7 = [GS2,D3,B2,D3,GS2,D3,B2,D3,GS2,D3,B2,D3,FS2,D3,B2,D3,ES2,D3,B2,D3,D2,D3,B2,D3,CS2,ES2,CS3,GS2,CS3,GS2,CS3,ES3]
+notes = len(pitches7)
+duration7 = [QN]*notes
 
 #partes inicio
 phrase1 = Phrase(0.0)
@@ -31,12 +39,16 @@ phrase2.addNoteList(pitches2, duration2)
 phrase3 = Phrase()
 phrase3.addNoteList(pitches3, duration3)
 
-phrase4 = Phrase()
-phrase4.addNoteList(pitches4, duration4)
+phrase6 = Phrase()
+phrase6.addNoteList(pitches6, duration6)
+
+phrase7 = Phrase()
+phrase7.addNoteList(pitches7, duration7)
 
 #duraciones
 intro_duration = len(pitches1) + len(pitches2) + len(pitches3)
 chorus_repetition = intro_duration + len(pitches4)
+intro_chorus_duration = intro_duration*2
 
 #repeticion melodia principal para coro
 phrase1_chorus = Phrase(intro_duration)
@@ -51,8 +63,13 @@ phrase3_chorus.addNoteList(pitches3, duration3)
 chorusCompany_phrase = Phrase(intro_duration)
 chorusCompany_phrase.addNoteList(pitches4, duration4)
 chorusCompany_phrase.addNoteList(pitches5, duration5)
+#melodía 2da parte
+mainMelody2_phrase = Phrase(intro_chorus_duration)
+mainMelody2_phrase.addNoteList(pitches6, duration6)
+mainMelody2_phrase.addNoteList(pitches7, duration7)
 
 mainMelody = Part("Main", PIANO, 0)
+
 #intro
 mainMelody.add(phrase1)
 mainMelody.add(phrase2)
@@ -61,13 +78,18 @@ mainMelody.add(phrase3)
 mainMelody.add(phrase1_chorus)
 mainMelody.add(phrase2_chorus)
 mainMelody.add(phrase3_chorus)
+#outro
+mainMelody2 = Part("Main2", CELLO, 2)
+mainMelody2.add(mainMelody2_phrase)
 
-chorusCompany = Part("Harmony", CELLO, 1)
+
+chorusCompany = Part("Harmony", CHOIR, 1)
 chorusCompany.add(chorusCompany_phrase)
 
 theme = Score("GLACEIR",145)
 theme.add(mainMelody)
 theme.add(chorusCompany)
-Play.setVolume(70, 1)
+theme.add(mainMelody2)
+Play.setVolume(90, 1)
 
 Play.midi(theme)
